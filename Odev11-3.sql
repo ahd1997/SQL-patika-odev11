9858a7dc868de75cfaacb5ab2d8cfd84f3d5c416
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 13:49:09 +0300

    Update Odev11-3.sql

--- Odev11-3.sql
+++ Odev11-3.sql
@@ -1,1 +1,2 @@
 --dvdrental örnek veri tabanı üzerinden actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+(SELECT first_name FROM actor) EXCEPT (SELECT first_name FROM customer);
