a47d59c996bdd971f9a56a087a16d167910f9f88
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 13:46:56 +0300

    Update Odev11-2.sql

--- Odev11-2.sql
+++ Odev11-2.sql
@@ -1,1 +1,2 @@
 --dvdrental örnek veri tabanı üzerinden actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+(SELECT first_name FROM actor) INTERSECT (SELECT first_name FROM customer);
