017cf015727945ff6ba19674e762de8af325c3f4
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 13:46:03 +0300

    Update Odev11-1.sql

--- Odev11-1.sql
+++ Odev11-1.sql
@@ -1,1 +1,2 @@
 --dvdrental örnek veri tabanı üzerinden actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+(SELECT first_name FROM actor) UNION (SELECT first_name FROM customer);
