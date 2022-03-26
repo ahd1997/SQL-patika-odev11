2c696e19f2fa195d7a41ae1a4d27fb1a15a1b71b
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 13:50:20 +0300

    Update Odev11-4.sql

--- Odev11-4.sql
+++ Odev11-4.sql
@@ -1,6 +1,7 @@
 --dvdrental örnek veri tabanı üzerinden
 --actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım ve tekrar eden veriler için de yapalım.
-
+(SELECT first_name FROM actor) UNION ALL (SELECT first_name FROM customer);
 --actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım ve tekrar eden veriler için de yapalım.
-
+(SELECT first_name FROM actor) INTERSECT ALL (SELECT first_name FROM customer);
 --actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım ve tekrar eden veriler için de yapalım.
+(SELECT first_name FROM actor) EXCEPT ALL (SELECT first_name FROM customer);
