889008090af52e1f8e45cc6b2e85f716418a066e
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-26 13:38:20 +0300

    Create Odev11-4.sql

---
+++ Odev11-4.sql
@@ -0,0 +1,6 @@
+--dvdrental örnek veri tabanı üzerinden
+--actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım ve tekrar eden veriler için de yapalım.
+
+--actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım ve tekrar eden veriler için de yapalım.
+
+--actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım ve tekrar eden veriler için de yapalım.
